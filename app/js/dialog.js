795a00079ad3c1a3629bf605a68bd63f971917bb
weissrobin <105072073+WeissRobin@users.noreply.github.com>
2023-06-04 16:55:20 +0200

    Added CSS

---
+++ app/js/dialog.js
@@ -0,0 +1,14 @@
+const modalBook = document.getElementById('modal-book-form');
+const newBookBtn = document.getElementById('new-book-btn');
+const closeBookBtn = document.getElementById('close-book-btn');
+const modalBox = document.querySelector('#modal-box');
+
+newBookBtn.addEventListener('click', () => {
+    modalBox.setAttribute('data-active', 'true');
+    modalBook.showModal();
+});
+
+closeBookBtn.addEventListener('click', () => {
+    modalBox.setAttribute('data-active', 'false');
+    modalBook.close();
+});
